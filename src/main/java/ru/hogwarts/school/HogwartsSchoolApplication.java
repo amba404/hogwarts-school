0cddf2fe765674c3ad2997a17469e306a6d169d5
amba404 <amba.avg@gmail.com>
2025-02-09 13:17:42 +0900

    swagger-ui was gone/ Return again/ Dont know why

--- src/main/java/ru/hogwarts/school/HogwartsSchoolApplication.java
+++ src/main/java/ru/hogwarts/school/HogwartsSchoolApplication.java
@@ -1,9 +1,11 @@
 package ru.hogwarts.school;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "Hogwarts School API", version = "1.0.0", description = "API for Hogwarts School"))
 public class HogwartsSchoolApplication {
 
     public static void main(String[] args) {
