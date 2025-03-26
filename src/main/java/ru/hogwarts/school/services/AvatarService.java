99cba81f3b61a637d653f724068331c8e062cc82
amba404 <amba.avg@gmail.com>
2025-03-26 14:09:00 +0900

    step 1

--- src/main/java/ru/hogwarts/school/services/AvatarService.java
+++ src/main/java/ru/hogwarts/school/services/AvatarService.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.services;
 
 import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AvatarService {
@@ -23,12 +26,16 @@ public class AvatarService {
     @Value(value = "${path.to.avatars}")
     private String avatarDirectory;
 
+    Logger logger = LoggerFactory.getLogger(AvatarService.class);
+
     public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
         this.avatarRepository = avatarRepository;
         this.studentService = studentService;
     }
 
     public Long uploadAvatar(Long studentId, @NotNull MultipartFile mFile) throws IOException {
+        logger.info("uploadAvatar: Upload avatar for student {}", studentId);
+
         Student student = studentService.getStudent(studentId);
 
         Path filePath = Path.of(avatarDirectory, studentId + "." + getExtension(mFile.getOriginalFilename()));
@@ -58,16 +65,28 @@ public class AvatarService {
     }
 
     public Avatar findAvatarOrNew(Long studentId) {
+        logger.info("findAvatarOrNew: Find avatar for student or create new {}", studentId);
+
         return avatarRepository.findByStudentId(studentId)
                 .orElseGet(Avatar::new);
     }
 
     public Avatar findAvatarOrFail(Long studentId) {
-        return avatarRepository.findByStudentId(studentId)
-                .orElseThrow(() -> new NotFoundException(String.format("Avatar for student %d not found", studentId)));
+        logger.info("findAvatarOrNew: Find avatar for student or throws exception {}", studentId);
+
+        Optional<Avatar> byStudentId = avatarRepository.findByStudentId(studentId);
+
+        if (byStudentId.isEmpty()) {
+            logger.error("findAvatarOrNew: avatar for student {} not found", studentId);
+            throw new NotFoundException(String.format("Avatar for student %d not found", studentId));
+        }
+
+        return byStudentId.get();
     }
 
     public List<Avatar> getAllPaged(int pageNumber, int pageSize) {
+        logger.info("getAllPaged: Get all avatars, page {}, size {}", pageNumber, pageSize);
+
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, pageSize);
         return avatarRepository.findAll(pageRequest).getContent();
     }
