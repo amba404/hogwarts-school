1552f6bd327e2e2cc5c1b7f0fcd996a9f6613de2
amba404 <amba.avg@gmail.com>
2025-03-26 14:43:29 +0900

    step 2

---
+++ src/main/java/ru/hogwarts/school/controllers/InfoController.java
@@ -0,0 +1,23 @@
+package ru.hogwarts.school.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.services.InfoService;
+
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
+
+    @GetMapping("/port")
+    public String getPort() {
+        return infoService.getPort();
+    }
+}
